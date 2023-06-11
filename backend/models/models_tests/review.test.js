704dd7588d5a75637358f1d25d0015f5d252b7ee
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-06-11 14:47:34 -0500

    wrote tests for review model and related functions

---
+++ backend/models/models_tests/review.test.js
@@ -0,0 +1,118 @@
+"use strict";
+
+const {
+  NotFoundError,
+  BadRequestError
+} = require("../../expressError.js");
+const Review = require("../review.js");
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+  testUserIds
+} = require("./common.js");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+
+
+//************ Tests ************
+
+describe('finds all reviews made FOR a user', function() {
+    test('works', async function() {
+        const result = await Review.getForUser(testUserIds[0]);
+        expect(result.length).toBe(1);
+
+        const result2 = await Review.getForUser(testUserIds[1]);
+        expect(result2.length).toBe(2);
+
+        const result3 = await Review.getForUser(testUserIds[2]);
+        expect(result3.length).toBe(1);
+    })
+
+    test('error if invalid user id', async function() {
+        try{
+            await Review.getForUser(testUserIds[4]);
+        } catch(err) {
+            expect(err instanceof NotFoundError).toBeTruthy();
+        }
+    })
+})
+
+describe('finds all reviews made BY a user', function() {
+    test('works', async function() {
+        const result = await Review.getFromUser(testUserIds[0]);
+        expect(result.length).toBe(1);
+
+        const result2 = await Review.getFromUser(testUserIds[1]);
+        expect(result2.length).toBe(2);
+
+        const result3 = await Review.getFromUser(testUserIds[2]);
+        expect(result3.length).toBe(1);
+    })
+
+    test('error if invalid user id', async function() {
+        try{
+            await Review.getFromUser(testUserIds[4]);
+        } catch(err) {
+            expect(err instanceof NotFoundError).toBeTruthy();
+        }
+    })
+})
+
+describe('creates a new review', function() {
+    test('works', async function() {
+        const newReviewData = {
+            title: 'rt4',
+            body: 'rb4',
+            stars: 5,
+            reviewed_by: testUserIds[0],
+            reviewed_for: testUserIds[1]
+        }
+
+        const result = await Review.create(newReviewData);
+        expect(result).toEqual({
+            'title': 'rt4',
+            'body': 'rb4',
+            'stars': 5,
+            'reviewed_by': testUserIds[0],
+            'reviewed_for': testUserIds[1]
+        })
+    })
+
+    test('error if invalid user id being reviewed', async function() {
+        const newReviewData = {
+            title: 'rt4',
+            body: 'rb4',
+            stars: 5,
+            reviewed_by: testUserIds[0],
+            reviewed_for: testUserIds[4]
+        }
+
+        try {
+            const result = await Review.create(newReviewData);
+        } catch(err) {
+            expect(err instanceof BadRequestError).toBeTruthy();
+        }
+    
+    })
+})
+
+describe('gets average review rating of user', function() {
+    test('works', async function() {
+        const result = await Review.getAverageRating(testUserIds[1]);
+        expect(result).toEqual(1.5)
+    })
+
+    test('error if no reviews available for this user', async function() {
+        try {
+            const result = await Review.getAverageRating(testUserIds[4]);
+        } catch(err) {
+            expect(err instanceof NotFoundError).toBeTruthy();
+        }
+    })
+})
