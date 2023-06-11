a4c732e8ca1ddd8d0bcd74fe8c89a852a588c91f
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-06-11 06:40:54 -0500

    wrote model for Job and related functions

--- backend/models/job.js
+++ backend/models/job.js
@@ -1,12 +1,154 @@
 "use strict";
 
 const db = require("../db.js");
-const {
-    NotFoundError,
-    BadRequestError,
-    UnauthorizedError,
-} = require("../expressError.js");
+const { NotFoundError } = require("../expressError.js");
 
 class Job {
     
+    /** Find all jobs in the database */
+
+    static async findAll(){
+        const result = await db.query(
+            `SELECT id,
+                    title, 
+                    body, 
+                    status, 
+                    address,
+                    posted_by, 
+                    assigned_to, 
+                    start_time, 
+                    end_time, 
+                    payment_due, 
+                    before_image_url, 
+                    after_image_url
+            FROM jobs
+            ORDER BY title`
+    )
+    return result.rows;
+    }
+
+    //find one
+    static async get(id){
+        const result = await db.query(
+            `SELECT id,
+                    title, 
+                    body, 
+                    status, 
+                    address,
+                    posted_by, 
+                    assigned_to, 
+                    start_time, 
+                    end_time, 
+                    payment_due, 
+                    before_image_url, 
+                    after_image_url
+            FROM jobs
+            WHERE id = $1`,
+            [id]
+        )
+
+        const job = result.rows[0];
+
+        if(!job) {
+            throw new NotFoundError(`Job not found with id: ${id}`);
+        }
+
+        return job;
+    }
+
+    //create job
+
+    static async create({title, body, address, posted_by, before_image_url}){
+        const result = db.query(
+            `INSERT INTO jobs(
+                title = $1, 
+                body = $2, 
+                status, 
+                address = $3,
+                posted_by = $4, 
+                assigned_to, 
+                start_time, 
+                end_time, 
+                payment_due, 
+                before_image_url = $5, 
+                after_image_url)
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING title, 
+                      body, 
+                      status, 
+                      address, 
+                      posted_by AS "postedBy, 
+                      assigned_to AS "assignedTo,
+                      start_time AS "startTime", 
+                      end_time AS "endTime", 
+                      payment_due AS "paymentDue, 
+                      before_image_url AS "beforeImage", 
+                      after_image_url) AS "afterImage"`,
+            [title, body, address, posted_by, before_image_url]
+        )
+
+        const job = result.rows[0];
+
+        return job
+    }
+
+    //update
+    static async update(id, data){
+
+        const setCols = []
+        const setVals = []
+        // starting index for SET placeholder values
+        let idx = 1;
+
+        // iterate over keys in data
+        for(let key of Object.keys(data)){
+            // push key and index to setCols with form 'key = $idx'
+            setCols.push(`${key} = $${idx}`)
+            // push values of data to setVals
+            setVals.push(data[key])
+            // increment idx after each loop
+            idx++;
+        }
+        // push the id to match in the WHERE statement into the setVals array
+        setVals.push(id)
+        
+        // build the query string from setCols and placeholder values, return the data that was updated 
+        const queryString = 
+            `UPDATE jobs SET ${setCols.join(", ")}
+             WHERE id = $${idx}
+             RETURNING ${setCols.join(", ")}`
+
+        // query the database with the query string and array of values to insert
+        const result = await db.query(queryString, setVals);
+
+        const job = result.rows[0];
+        if(!job){
+            throw new NotFoundError(`No job found with id: ${id}`)
+        }
+
+        return job;
+    }
+    
+    //remove
+    static async remove(id){
+        const result = await db.query(
+            `DELETE from jobs
+            WHERE id = $1
+            RETURNING id`,
+            [id]
+        )
+
+        const job = result.rows[0];
+
+        if(!job){
+            throw new NotFoundError(`No job found with id: ${id}`)
+        }
+    }
+
 }
+
+
+
+
+
+module.exports = Job;
