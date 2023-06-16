9de2f8164917f776340972bab6fa095cdbe31890
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-06-16 07:03:39 -0500

    removed console.logs

--- backend/helpers/partialSqlUpdate.js
+++ backend/helpers/partialSqlUpdate.js
@@ -1,10 +1,9 @@
 "use strict";
 
 function partialUpdate(data){
-    console.log(data)
+
     const setCols = Object.keys(data).map((key, idx) => `${key} = $${idx + 1}`)
-    
-    console.log(setCols)
+
     return {
             setCols: setCols.join(", "),
             setVals: Object.values(data)
