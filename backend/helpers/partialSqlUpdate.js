73f9fef99b7f9d9fef7aab1db421632e9da99305
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-06-17 06:30:15 -0500

    added logic to remove id from data passed to function

--- backend/helpers/partialSqlUpdate.js
+++ backend/helpers/partialSqlUpdate.js
@@ -1,6 +1,10 @@
 "use strict";
 
 function partialUpdate(data){
+    // if id was passed in data, delete it before mapping columns
+    if(data.id) {
+        delete data.id;
+    }
 
     const setCols = Object.keys(data).map((key, idx) => `${key} = $${idx + 1}`)
 
