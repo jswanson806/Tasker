b6db974e90fd8d3946a679feba44a61b9f51c136
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-06-20 06:54:37 -0500

    uncommented router for messages

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 
 const { NotFoundError } = require('./expressError.js')
 
-const authRoutes = require("./routes/auth");
-const usersRoutes = require("./routes/users");
-const jobsRoutes = require("./routes/jobs");
-const messagesRoutes = require("./routes/messages");
-const payoutsRoutes = require("./routes/payouts");
-const reviewsRoutes = require("./routes/reviews");
+// const authRoutes = require("./routes/auth");
+const usersRoutes = require("./routes/users.js");
+const jobsRoutes = require("./routes/jobs.js");
+const messagesRoutes = require("./routes/messages.js");
+
+// const payoutsRoutes = require("./routes/payouts");
+// const reviewsRoutes = require("./routes/reviews");
 
 const morgan = require("morgan");
 
@@ -18,12 +19,12 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
-app.use("/auth", authRoutes);
+// app.use("/auth", authRoutes);
 app.use("/users", usersRoutes);
 app.use("/jobs", jobsRoutes);
 app.use("/messages", messagesRoutes);
-app.use("/payouts", payoutsRoutes);
-app.use("/reviews", reviewsRoutes);
+// app.use("/payouts", payoutsRoutes);
+// app.use("/reviews", reviewsRoutes);
 
 /** handle 404 errors; matches everything */
 app.use(function (req, res, next) {
