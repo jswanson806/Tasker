c4631c3c6aaced4c910122cb6e9c11933ea360ab
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-07-15 06:48:46 -0500

    added authentication routes for registering and logging in

---
+++ backend/routes/auth.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = new express.Router();
+const {BadRequestError} = require('../expressError.js');
+const jsonschema = require("jsonschema");
+const { createToken } = require("../helpers/tokens.js");
+const userAuthSchema = require('../schemas/userAuthSchema.json');
+const userSchema = require('../schemas/userSchema.json');
+const User = require('../models/user.js');
+
+
+/** POST /auth/token:  { username, password } => { token }
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ * Authorization required: none
+ */
+
+router.post("/token", async function (req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, userAuthSchema);
+        if(!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+
+        const { username, password } = req.body;
+        const user = await User.authenticate(username, password);
+        const token = createToken(user);
+        return res.json({token});
+    } catch(err) {
+        return next(err);
+    }
+});
+
+
+/** POST /auth/register:   { user } => { token }
+ *
+ * user must include { username, password, firstName, lastName, email }
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ * Authorization required: none
+ */
+
+router.post("/register", async function (req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, userSchema);
+        if(!validator.valid){
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+
+        const newUser = await User.register({...req.body, isAdmin: false, isWorker: false})
+        const token = createToken(newUser);
+        return res.status(201).json({token});
+    } catch(err) {
+        return next(err);
+    }
+})
+
+module.exports = router;
