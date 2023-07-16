d11a0313a4865965ecb82d99283158ae26b40601
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-07-16 14:02:13 -0500

    added docstring and text for the form button

--- frontend/tasker/src/components/Login.js
+++ frontend/tasker/src/components/Login.js
@@ -1,8 +1,21 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import TaskerApi from "../api";
+
+/**
+ * Login component for user authentication.
+ *
+ * This component renders a login form where users can enter their email and password
+ * to authenticate themselves. Upon successful login, the user's token is stored in
+ * the local storage for further authentication. The user is then redirected to the
+ * dashboard.
+ *
+ * @returns {JSX.Element} The rendered login form component.
+ */
 
 const Login = () => {
 
+    
     const navigate = useNavigate();
 
     const INITIAL_STATE = {
@@ -20,7 +33,7 @@ const Login = () => {
         }))
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         const userInfo = {
@@ -28,9 +41,17 @@ const Login = () => {
             password: formData.password,
         }
 
-        // login user call goes here
-        
+        const res = await TaskerApi.getAllJobs();
+        console.log(res)
+
+        // // call api to login the user
+        // const token = await TaskerApi.login(userInfo.email, userInfo.password);
+        // // set the returned token in localStorage for further authentication
+        // localStorage.setItem('userToken', token)
+
+        // clear the form data
         setFormData(INITIAL_STATE);
+        // redirect to dashboard
         navigate("/dashboard");
     }
 
@@ -46,6 +67,7 @@ const Login = () => {
                         type="text"
                         name="email"
                         placeholder="Email Address"
+                        data-testid="login-form-email-input"
                         value={FormData.email}
                         onChange={handleChange}
                     />
@@ -55,9 +77,11 @@ const Login = () => {
                         type="password"
                         name="password"
                         placeholder=""
+                        data-testid="login-form-password-input"
                         value={FormData.password}
                         onChange={handleChange}
                     />
+                    <button type="submit" data-testid="login-form-button">Login</button>
                 </form>
             </div>
         </div>
