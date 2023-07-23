e88cf2f9a302897699332a597ca046f9f455a386
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-07-23 08:10:36 -0500

    added skeleton of componenets rendered by Router.js

---
+++ frontend/tasker/src/components/Applications.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Application = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Application;
---
+++ frontend/tasker/src/components/CreateJob.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CreateJob = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default CreateJob;
