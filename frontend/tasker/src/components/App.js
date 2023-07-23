09bbc70e10c2d9759617d8a5a30b16c451fca971
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-07-23 08:11:32 -0500

    wrapped rendered componenets in TokenContext

--- frontend/tasker/src/components/App.js
+++ frontend/tasker/src/components/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
 import Router from './Router.js';
+import NavBar from './NavBar.js';
 import './styles/App.css';
+import { TokenProvider } from '../helpers/TokenContext.js';
+
+/** App Component
+ * 
+ * Handles the rendering of the NavBar and Router components.
+ * 
+ * Uses TokenProvider to provide global access to the token
+ * 
+ */
 
 function App() {
+
   return (
-    <div className="App">
-      <Router />
-    </div>
+        <TokenProvider>
+          <NavBar />
+          <Router />
+        </TokenProvider>
   );
 }
 
