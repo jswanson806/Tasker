41ef2c6011a7dba6acb94a57c73928d3882df001
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-07-30 13:56:05 -0500

    refactored Jobs for readability. Moved function declarations outside of useEffect hooks, added dependencies for hook that calls createJobCards, added conditional logic based on isWorker property of the current user to hide or show certain elements depending on what the user is looking at on the page

--- frontend/tasker/src/components/Jobs.js
+++ frontend/tasker/src/components/Jobs.js
@@ -5,38 +5,112 @@ import { UserContext } from '../helpers/UserContext';
 
 const Jobs = () => {
 
-    const [jobs, setJobs] = useState([]);
-    const [jobCards, setJobCards] = useState([]);
     const {user} = useContext(UserContext)
 
+    const [jobs, setJobs] = useState([]);
+    const [jobCards, setJobCards] = useState([]);
+    const [showUserJobs, setShowUserJobs] = useState(false);
+    const [currUser, setCurrUser] = useState(null);
+    
+    // on initial render, calls functions to set state of currUser and jobs
     useEffect(() => {
-        /** Retrieves all jobs from api
-         * 
-         * Sets state of jobs
-         */
-        async function fetchAndSetJobs() {
-            try{
-                const jobsRes = await TaskerApi.getAllJobs();
-                const { allJobs } = jobsRes;
-                setJobs(allJobs);
-            } catch(err) {
-                return err;
-            }
+
+        fetchCurrUser(JSON.parse(user).id);
+        fetchAndSetJobs();
+        
+    }, []);
+
+
+    // creates job cards if currUser and jobs are populated.
+    useEffect(() => {
+        
+        // only call once jobs have been populated
+        if(jobs.length && currUser) {
+            createJobCards();
         }
+        
+    }, [jobs, showUserJobs, currUser])
 
-        fetchAndSetJobs();  
-    },[]);
 
-    useEffect(() => {
+    /**
+    * Fetches the current user's data, filters out already applied jobs,
+    * and creates JobCard components for each unapplied job in the 'jobs' list.
+    *
+    */
+    async function createJobCards(){
+        // create Set from existing user applications
+        const applications = new Set(currUser.applications);
+        
+        // sets state of jobCards based on filtering logic related to user applications
         setJobCards(jobs.map((job) => {
-            return <JobCard user={JSON.parse(user)} job={job} key={job.id}/>
+            // filter out jobs to which user has already applied
+            if(!applications.has(job.id) && !showUserJobs) {
+                return <JobCard user={JSON.parse(user)} job={job} fetchCurrUser={fetchCurrUser} key={job.id}/>
+            }
+            // filter out jobs to which user has not already applied
+            if(applications.has(job.id) && showUserJobs) {
+                return <JobCard user={JSON.parse(user)} job={job} fetchCurrUser={fetchCurrUser} key={job.id}/>
+            }
         }));
-    }, [jobs])
+    }
+
+
+    /**
+    * Fetches data for a single user from the Tasker API based on the given ID.
+    *
+    * @async
+    * @function fetchCurrUser
+    * @param {number} id - The ID of the user to fetch.
+    * @returns {Promise<object>} A Promise that resolves with the user data as an object.
+    * @throws {Error} If there's an error during the API call or the user data cannot be retrieved.
+    */
+    async function fetchCurrUser(id){
+        try {
+            const res = await TaskerApi.getSingleUser(id);
+            const { user } = res;
+            setCurrUser(user);
+        } catch(err) {
+            throw err;
+        }
+    }
+
+
+    /** Retrieves all jobs from api
+    * 
+    * Sets state of jobs
+    */
+    async function fetchAndSetJobs() {
+        try{
+            const jobsRes = await TaskerApi.getAllJobs();
+            const { allJobs } = jobsRes;
+            setJobs(allJobs);
+        } catch(err) {
+            return err;
+        }
+    }
+
+    /** Inverts the state of showUserJobs 
+     * 
+     * Sets state of showUserJobs
+    */
+    const toggleUserJobs = () => {
+        setShowUserJobs(!showUserJobs);
+    }
+
+    // conditionally renders a button element depending on isWorker property of user
+    const toggleJobsButton = JSON.parse(user).isWorker === true 
+        ? 
+        <button onClick={toggleUserJobs}>{showUserJobs ? 'All Jobs' 
+        : 
+        'My Jobs'}</button> : '';
 
 
     return (
         <div>
-            <h1>Jobs</h1>
+            <h1>{showUserJobs ? 'My Jobs' : 'All Jobs'}</h1>
+            <div>
+                {toggleJobsButton}
+            </div>
             <div>
                 {jobCards}
             </div>
