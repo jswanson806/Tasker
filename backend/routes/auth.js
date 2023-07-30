b313e7a3dfce1d341e594d99b2a20e9cf42e63e2
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-07-30 13:51:31 -0500

    altered logic upon registering user to receive a value from isWorker property of user object instead of explicitly setting this value

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -52,7 +52,7 @@ router.post("/register", async function (req, res, next) {
 
         const { user } = req.body;
 
-        const newUser = await User.register({...user, isAdmin: false, isWorker: false})
+        const newUser = await User.register({...user, isAdmin: false})
         console.log(newUser)
         const token = createToken(newUser);
         return res.status(201).json({token});
