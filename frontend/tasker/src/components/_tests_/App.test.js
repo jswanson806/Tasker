1ea25d6ec91ca23d43cb5c54dffd79966e40c088
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-08-06 09:07:01 -0500

    wrote tests for App component

--- frontend/tasker/src/components/_tests_/App.test.js
+++ frontend/tasker/src/components/_tests_/App.test.js
@@ -1,8 +1,64 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
+import App from '../App.js';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('smoke and snapshot tests',() => {
+
+  test('renders without crashing', () => {
+
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+  });
+
+
+  test('matches the snapshot', () => {
+
+    let asFragment;
+
+    const {asFragment: fragment } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    asFragment = fragment;
+
+    expect(asFragment()).toMatchSnapshot
+
+  });
+
+})
+
+describe('renders correct components', () => {
+  
+  test('renders Router component', () => {
+
+    const {container } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(container).toHaveTextContent('Welcome to Tasker');
+
+  })
+
+  test('renders NavBar component', () => {
+
+    const {container } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(container).toHaveTextContent('Login');
+    expect(container).toHaveTextContent('Sign Up');
+
+  })
+
+})
+
