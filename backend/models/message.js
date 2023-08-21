912c6d87c206884f28c18df832e29ac01e8cda85
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-08-21 05:25:18 -0500

    altered methods to use job_id as argument in order to concat. custom conversation ids which include the job id and both user ids

--- backend/models/message.js
+++ backend/models/message.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const db = require("../db.js");
-const { NotFoundError, BadRequestError } = require("../expressError.js");
+const { NotFoundError } = require("../expressError.js");
 
 class Message {
 
@@ -13,12 +13,12 @@ class Message {
      * Returns messages as [{id, body, conversation_id, sent_by, sent_to, created_at}, ...]
      *  ordered from latest to oldest message
     */
-    static async getConversation(userId1, userId2){
+    static async getConversation(user_id1, user_id2, job_id){
         // get unique id for conversation
-        let convoId = this.generateConvoId(userId1, userId2)
+        let convoId = this.generateConvoId(user_id1, user_id2, job_id)
 
         const result = await db.query(
-            `SELECT m.body, m.created_at
+            `SELECT m.body, TO_CHAR(m.created_at, 'MM/DD/YYYY HH:MI AM') AS created_at
             FROM messages AS m
             JOIN conversations AS c ON m.conversation_id = c.id
             WHERE c.id = $1
@@ -29,7 +29,7 @@ class Message {
         const messages = result.rows;
 
         if(messages.length === 0){ // throws error if no conversation exists between these users
-            throw new NotFoundError(`No messages found between users: ${userId1} and ${userId2}`)
+            throw new NotFoundError(`No messages found`)
         }
 
         return messages;
@@ -40,9 +40,9 @@ class Message {
      * 
      * Returns message {body, conversation_id, sent_by, sent_to, created_at}
     */
-    static async create({body, sent_by, sent_to}){
+    static async create({body, sent_by, sent_to, job_id}){
         // get unique id for conversation
-        let convoId = this.generateConvoId(sent_by, sent_to)
+        let convoId = this.generateConvoId(sent_by, sent_to, job_id)
 
         // query existing conversation from db
         const existingConvo = await db.query(
@@ -75,12 +75,12 @@ class Message {
     /** Make unique id from two user id's
      * 
      * concatenates user ids to form unique id to match to conversations table in form
-     *  < u + lower id value + u + greater id value >
+     *  < u + lesser id value + u + greater id value + job_id>
      * 
      * Returns unique id as string
      */
-    static generateConvoId(id1, id2){
-        let uniqueId = id1 > id2 ? `u${id2}u${id1}` : `u${id1}u${id2}`;
+    static generateConvoId(user_id1, user_id2, job_id){
+        let uniqueId = user_id1 > user_id2 ? `u${user_id2}u${user_id1}j${job_id}` : `u${user_id1}u${user_id2}j${job_id}`;
         return uniqueId;
     }
 
