33c67c78ae0cfada5d8389545e0c0385b46d3ab6
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-09-08 06:34:50 -0500

    added method to get the most recent conversation messages involving a particular user. This is used to display the conversation previews on the messages page

--- backend/models/message.js
+++ backend/models/message.js
@@ -18,18 +18,109 @@ class Message {
         let convoId = this.generateConvoId(user_id1, user_id2, job_id)
 
         const result = await db.query(
-            `SELECT m.body, TO_CHAR(m.created_at, 'MM/DD/YYYY HH:MI AM') AS created_at
+            `SELECT m.id, 
+                    m.body, 
+                    TO_CHAR(m.created_at, 'MM/DD/YYYY HH:MI AM') AS created_at,
+                    sent_by,
+                    sent_to
             FROM messages AS m
             JOIN conversations AS c ON m.conversation_id = c.id
             WHERE c.id = $1
-            ORDER BY m.created_at DESC`,
+            ORDER BY m.created_at ASC`,
             [convoId]
         )
 
         const messages = result.rows;
 
-        if(messages.length === 0){ // throws error if no conversation exists between these users
-            throw new NotFoundError(`No messages found`)
+        if(messages.length === 0){ // does not throw error if no conversation exists between these users
+            return null;
+        }
+
+        return messages;
+    }
+
+    /** Find all messages between two users 
+     * 
+     * concatenates user ids to form unique id to match to conversations table in form
+     *  < u + lower id value + u + greater id value >
+     * 
+     * Returns messages as [{id, body, conversation_id, sent_by, sent_to, created_at}, ...]
+     *  ordered from latest to oldest message
+    */
+    static async getAllConversationsInvolving(id){
+
+        const result = await db.query(
+            `SELECT m.body, TO_CHAR(m.created_at, 'MM/DD/YYYY HH:MI AM') AS created_at
+            FROM messages AS m
+            JOIN conversations AS c ON m.conversation_id = c.id
+            WHERE c.id LIKE $1 OR c.id LIKE $2
+            ORDER BY m.created_at DESC`,
+            [`u${id}%`, `%u${id}%`]
+        )
+
+        const conversations = result.rows;
+
+        if(conversations.length === 0){ // does not throw error if no conversation exists between these users
+            return null;
+        }
+
+        return messages;
+    }
+
+
+    /** Find all messages involving a particular user id 
+     * 
+     * Returnes messages as [{id, body, conversation_id, sent_by, sent_to, created_at}, ...]
+     *  ordered from latest to oldest message
+    */
+
+    static async getAllMessagesInvolving(id){
+        const result = await db.query(
+            `SELECT id,
+                    body, 
+                    sent_to AS sentTo,
+                    sent_by AS sentBy,
+                    conversation_id AS conversationId, 
+                    TO_CHAR(created_at, 'MM/DD/YYYY HH:MI AM') AS created_at
+            FROM messages
+            WHERE conversation_id LIKE $1 OR conversation_id LIKE $2
+            ORDER BY created_at ASC`,
+            [`u${id}%`, `%u${id}%`]
+        )
+
+        const messages = result.rows;
+
+        if(messages.length === 0){
+            return null;
+        }
+
+        return messages; 
+
+    }
+
+
+    static async getRecentConvoMessagesInvolving(id) {
+        const result = await db.query(
+            `SELECT conversation_id AS convo_id,
+                    id,
+                    body,
+                    sent_by,
+                    sent_to,
+                    TO_CHAR(created_at, 'MM/DD/YYYY HH:MI AM') AS created_at
+            FROM messages AS m
+            WHERE id = (
+                SELECT MAX(id)
+                FROM messages
+                WHERE conversation_id LIKE $1 OR conversation_id LIKE $2
+            )
+            GROUP BY convo_id, id, body, sent_by, sent_to, created_at`,
+            [`u${id}%`, `%u${id}%`]
+        )
+
+        const messages = result.rows;
+
+        if(messages.length === 0){
+            return null;
         }
 
         return messages;
