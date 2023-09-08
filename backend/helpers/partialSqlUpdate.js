8820b144328b4c18f90bbfdf74f44166ac1c2457
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-09-08 06:31:56 -0500

    added logic to remove applicants property when making sql query string

--- backend/helpers/partialSqlUpdate.js
+++ backend/helpers/partialSqlUpdate.js
@@ -6,6 +6,18 @@ function partialUpdate(data){
         delete data.id;
     }
 
+    if(data.applicants) {
+        delete data.applicants;
+    }
+
+    if(!data.end_time) {
+        data.end_time = null;
+    }
+
+    if(!data.start_time) {
+        data.start_time = null;
+    }
+
     const setCols = Object.keys(data).map((key, idx) => `${key} = $${idx + 1}`)
 
     return {
