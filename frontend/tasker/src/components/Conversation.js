4e112d54bb66a1f63e86932a55899cda6b2e7f33
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-09-30 10:22:10 -0500

    refactored to use reactstrap elements and imported style sheet

--- frontend/tasker/src/components/Conversation.js
+++ frontend/tasker/src/components/Conversation.js
@@ -1,13 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import NewMessageForm from './NewMessageForm.js';
+import { ModalBody, ModalHeader } from 'reactstrap';
+import "./styles/Conversation.css";
 
 
-const Conversation = ({user, currUser, messages, jobId, onMessageSent, convoId}) => {
+const Conversation = ({user, currUser, messages, jobId, onMessageSent, convoId, onClose}) => {
 
     const [convoTitle, setConvoTitle] = useState('');
 
-    useEffect(() => {
+    const messagesContainerRef = useRef(null);
 
+    useEffect(() => {
+        // When messages change, scroll to the bottom of the container
+        messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
+    }, [messages]);
+
+    useEffect(() => {
         setConvoTitle(generateConvoTitle(user));
         
     }, [])
@@ -20,24 +28,31 @@ const Conversation = ({user, currUser, messages, jobId, onMessageSent, convoId})
 
     return (
         <div className="convo-container">
+            <div className="convo-centered-card">
             {/* renders the name of the user that is being messaged as <firstName + lastName initial + '.'> */}
-            <h1>{convoTitle}</h1>
-            <div className="convo-messages-container">
-                {/* displays the messages when messages[0] is truthy */}
-                <div>{messages}</div>
-
-            </div>
+            <ModalHeader>{convoTitle}</ModalHeader>
+            <ModalBody style={{ maxHeight: "700px", display: "flex", flexDirection: "column-reverse", overflowY: "auto"}}>
             <div className="convo-new-message-form">
-                <NewMessageForm 
-                            convoId={convoId} 
-                            assignedUser={user} 
-                            currUser={currUser} 
-                            jobId={jobId} 
-                            onMessageSent={onMessageSent}
-                />
+                    <NewMessageForm 
+                        convoId={convoId} 
+                        assignedUser={user} 
+                        currUser={currUser} 
+                        jobId={jobId} 
+                        onMessageSent={onMessageSent}
+                        onClose={onClose}
+                    />
+                </div>
+                {/* displays the messages when messages[0] is truthy */}
+                <div 
+                    className="convo-messages-container"
+                    ref={messagesContainerRef}
+                >
+                    {messages}
+                </div>
+                
+                </ModalBody>
             </div>
         </div>
-        
     )
 }
 
