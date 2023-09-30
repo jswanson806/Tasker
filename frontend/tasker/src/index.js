6da7c12c130dd8701b53f36185060bd44052d870
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-09-30 10:30:36 -0500

    refactored to use reactstrap elements and imported style sheet

--- frontend/tasker/src/index.js
+++ frontend/tasker/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './components/App.js';
 import reportWebVitals from './reportWebVitals';
 import { TokenProvider } from './helpers/TokenContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
