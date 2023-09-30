5e3df07c9e1f175f6abe1a076b31c7b93f845f8a
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-09-30 10:21:56 -0500

    refactored to use reactstrap elements and imported style sheet

--- frontend/tasker/src/components/Confirmation.js
+++ frontend/tasker/src/components/Confirmation.js
@@ -1,19 +1,38 @@
 import React from "react";
+import {ModalHeader, ModalBody, Button} from "reactstrap";
+import "./styles/Confirmation.css"
 
 const Confirmation = ({targetUserId, job, onConfirm, onClose}) => {
 
     const handleConfirmation = () => {
-        console.log(targetUserId)
         onConfirm(job.id, targetUserId);
         onClose();
     }
 
 
     return (
-        <div>
-            <h3>Assign user to job?</h3>
-            <button onClick={handleConfirmation}>Confirm</button>
-            <button onClick={onClose}>Cancel</button>
+        <div className="confirmation-container">
+            <div className="confirmation-centered-card">
+                <ModalHeader>Are you sure you want to assign this applicant to your job?</ModalHeader>
+                <ModalBody>
+                    <Button 
+                        data-testid="confirmation-confirm-button" 
+                        className="button" 
+                        color="success" 
+                        onClick={handleConfirmation}
+                    >
+                        Confirm
+                    </Button>
+                    <Button 
+                        data-testid="confirmation-cancel-button" 
+                        className="button" 
+                        color="danger" 
+                        onClick={onClose}
+                    >
+                        Cancel
+                    </Button>
+                </ModalBody>
+            </div>
         </div>
     )
 };
