18344690c884778da7665d09e29e78ae6a1f5e33
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-10-03 06:46:11 -0500

    removed conditionals that added properties to objects

--- backend/helpers/partialSqlUpdate.js
+++ backend/helpers/partialSqlUpdate.js
@@ -1,21 +1,9 @@
 "use strict";
 
 function partialUpdate(data){
-    // if id was passed in data, delete it before mapping columns
-    if(data.id) {
-        delete data.id;
-    }
 
     if(data.applicants) {
         delete data.applicants;
-    }
-
-    if(!data.end_time) {
-        data.end_time = null;
-    }
-
-    if(!data.start_time) {
-        data.start_time = null;
     }
 
     const setCols = Object.keys(data).map((key, idx) => `${key} = $${idx + 1}`)
