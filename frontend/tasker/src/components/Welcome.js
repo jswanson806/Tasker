f7933a8f07e0ee184d7c74f91dcb79902def434f
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-10-15 14:54:28 -0500

    updated styles and included Nav elements from reactstrap

--- frontend/tasker/src/components/Welcome.js
+++ frontend/tasker/src/components/Welcome.js
@@ -1,15 +1,45 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
+import {Button, NavItem, Nav} from 'reactstrap';
+import "./styles/Welcome.css";
+
+
 
 const Welcome = () => {
+
+
     return(
-        <div>
+        <div className="welcome-card-container">
+            <div className="welcome-centered-card">
+
             <h1>Welcome to Tasker!</h1>
-            <p>Tasker is your personal marketplace for finding help to get things done. Sign up to post your open jobs or register as a Worker to get hired today!</p>
-            <p>New here?</p>
-            <a href='/signup'>Sign Up</a>
-            <br></br>
-            <p>Already registered?</p>
-            <a href='/login'>Login</a>
+
+                <h4>Tasker is your personal marketplace for finding help to get things done.</h4>
+                <h6>Sign up as a 'User' to post your open jobs or as a 'Worker' to get hired today!</h6>
+                
+                <div className="welcome-buttons-container">
+                    <h6>New here?</h6>
+                    <Nav className="ml-auto">
+                        <NavItem className="mr-2">
+                            <NavLink to="/signup" className="me-auto">
+                                <Button color="info" data-testid="welcome-signup-button">
+                                    Sign Up
+                                </Button>
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                    <h6>Already registered?</h6>
+                    <Nav className="ml-auto">
+                        <NavItem className="mr-2" >
+                            <NavLink to="/login" className="me-auto">
+                                <Button color="warning" className="welcome-login-button" data-testid="welcome-login-button">
+                                    Login
+                                </Button>
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </div>
+            </div>
         </div>
     );
 }
