5e2f0ab8491cbd52c79451e1613ec8c4d58d7954
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2024-03-10 07:05:12 -0500

    removed uncessary function call to authenticate user upon registering

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -53,7 +53,6 @@ router.post("/register", async function (req, res, next) {
         const { user } = req.body;
 
         const newUser = await User.register({...user, isAdmin: false})
-        await User.authenticate(user.email, user.password);
         const token = createToken(newUser);
         return res.status(201).json({token});
     } catch(err) {
